116b9ce7a40f467ce1bc282faefcd63c578ac57c
辰己功太郎 <tatsumi201702@gmail.com>
2017-09-17 15:22:54 +0900

    PinBall課題提出
    得点を表示させるように作業を行いました

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class ScoreController : MonoBehaviour
+{
+	public Text ScoreText; //Text用変数
+	private int score = 0; //スコア計算用変数
+
+	void Start()
+	{
+		score   = 0;
+		SetScore();   //初期スコアを代入して表示
+	}
+		
+	void OnCollisionEnter( Collision collision )
+	{
+		string yourTag  = collision.gameObject.tag;
+
+		if( yourTag == "LargeStarTag" )
+		{
+			score += 150;
+		}
+		else if( yourTag == "LargeCloudTag" )
+		{
+			score += 150;
+		}
+		else if( yourTag == "SmallCloudTag" )
+		{
+			score += 100;
+		}
+		else
+		{
+			score += 0;
+		}
+
+		SetScore();
+	}
+
+	void SetScore()
+	{
+		ScoreText.text = string.Format( "Score:{0}", score );
+	}
+}
